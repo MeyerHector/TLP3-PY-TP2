51b5bd8af5f869c76c5d4f8b818fdca17d4c4ff6
Meyer <meyer-hector@hotmail.com>
2024-04-12 11:11:26 -0300

    Optimizacion de rendimiento

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ from src.utils.create_csv  import create_csv
 def main():
     cursor, db = db_connect()
     with open('./src/data/localidades.csv', mode='r', encoding='utf-8', newline='\n') as archivo_csv:
-        lector_csv = csv.DictReader(archivo_csv)
+        lector_csv = csv.reader(archivo_csv)
         try:
-            for provincia in lector_csv:
-                sql = '''INSERT INTO `localidades`(`provincia`, `id`, `localidad`, `cp`, `id_prov_mstr`) 
-                        VALUES (%s, %s, %s, %s, %s)'''
-                values = (provincia['provincia'], provincia['id'], provincia['localidad'], provincia['cp'], provincia['id_prov_mstr'])
-                cursor.execute(sql, values)
+            sql = '''INSERT INTO `localidades`(`provincia`, `id`, `localidad`, `cp`, `id_prov_mstr`) 
+                        VALUES (%s, %s, %s, %s, %s)'''    
+            cursor.executemany(sql, lector_csv)
             db.commit()
             print('Localidades insertadas correctamente')
         except MySQLdb.error as e:
